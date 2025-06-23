addb8a2742e0b95eaad889a1c5231d92d179d89d
Rikuto <orikutobasuke@gmail.com>
2025-06-24 02:06:46 +0900

    create: create Button Component

--- .history/my-app/src/app/layout_20250624015042.tsx
+++ .history/my-app/src/app/layout_20250624015042.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../styles/globals.css";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- .history/my-app/src/app/page_20250624015327.tsx
+++ .history/my-app/src/app/page_20250624015327.tsx
@@ -1,12 +1,10 @@
 import { devIndicatorServerState } from "next/dist/server/dev/dev-indicator-server-state";
 import Image from "next/image";
-import Button from "@/components/Button";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
-      <Button />
     </div>
   );
 }
